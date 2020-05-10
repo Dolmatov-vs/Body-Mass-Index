1bde43c9357636253e4355647289d3ef56025008
Vitalii Dolmatov <dolmatov-vs@yandex.ru>
2020-05-10 10:59:06 +0300

    Edit main

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,6 +1,5 @@
 public class BmiService {
-    public double calculate(double height, double
-            mass) {
+    public double calculate(double height, double mass) {
         height = height/100;  //  Переводим рост в метры
         double bmi = (mass/(Math.pow(height,2)));
         return bmi;
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
-        double mass = 81.5;  // Масса в килограммах
-        int height = 175; // Рост в сантиметрах
-        double bmi = service.calculate(height, mass);
-                System.out.println(bmi);
+        double bmi = service.calculate(175, 75.5);
+                System.out.printf("Индекс массы тела = %.2f", bmi);
     }
 }
